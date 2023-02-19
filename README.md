b7493ba0d2354ff6ee8f640596f2898c2cc583e2
Jordan1819 <jmanwaite@gmail.com>
2023-02-18 17:15:06 -0700

    Final Updates

--- README.md
+++ README.md
@@ -1,25 +1,26 @@
 # Game-Framework
+Author: Jordan Waite
 
 # Overview
-This game is a simple platformer style game where the player jumps from platform to platform and attempts to avoid falling to the bottom of the screen, which results in a game over.
+This game is a simple platformer style game where the player navigates the screen, jumps over obstacles, and collects stars (points) while trying to avoid enemy characters. This project utilizes the Python arcade module and many of the built in resources and methods. The development of this software involved drawing to a window, using inbuilt resources, collision detection, physics, and user input control. 
 
 # How to Play:
-
+The user can begin playing this game by simply running the main.py file in a code editor. The program will then open a new window and begin a new game. The player character can be controlled with the traditional arrow keys or with the WASD keys. The goal of the game is to collect as many stars as possible before being hit by one of the enemies.
 
 # Purpose
-My purpose in creating this software was to experiment and learn more about game development, the general file structure of games and complex software, and the basics of creating more interactive software.
+My purpose in creating this software was to experiment and learn more about game development, the general file structure of games and complex software, and the basics of creating more interactive software. It has always been a dream of mine to independently develop, write, and publish a two dimensional retro RPG game with a meaningful story and immersive and complex mechanics. The old-school Final Fantasy games had a huge impact on my taste in video games, books, and my interest in programming in general. I would love to spark that interest and passion in someone else while also investing in a creative outlet through a medium that hones and improves my software development skills.
 [Software Demo Video](http://youtube.link.goes.here)
 
 # Development Environment
-I utilized visual studio code to create this software, and it is written in python and uses the arcade module.
+This project was created in Microsoft Visual Studio Code and it is written in the Python programming language with the "arcade" Python module add-on.
 
 # Useful Websites
-{Make a list of websites that you found helpful in this project}
-* [Web Site Name](http://url.link.goes.here)
-* [Web Site Name](http://url.link.goes.here)
+These websites were my primary sources for tutorials, information, troubleshooting, and general research throughout this project.
+* [Python Arcade](https://api.arcade.academy/en/latest/)
+* [Stack Overflow](https://stackoverflow.com/)
+* [Python Documentation](https://docs.python.org/3/contents.html)
 
 # Future Work
-{Make a list of things that you need to fix, improve, and add in the future.}
-* Item 1
-* Item 2
-* Item 3
+* Add a point counter on the top of the screen.
+* Add enemies that, if collided with, will initiate a GAME OVER screen and restart the game.
+* Implement a level system where the player advances to a more difficult level once they've gathered all of the stars in the current level.
--- main.py
+++ main.py
@@ -6,9 +6,16 @@ import arcade
 
 # Constants
 SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
-SCREEN_TITLE = "Platformer"
+SCREEN_HEIGHT = 700
+SCREEN_TITLE = "Jumpbot"
 
+CHARACTER_SCALING = 1
+TILE_SCALING = 0.5
+STAR_SCALING = 0.5
+
+PLAYER_MOVEMENT_SPEED = 5
+GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
 
 class game(arcade.Window):
     """
@@ -17,24 +24,148 @@ class game(arcade.Window):
 
     def __init__(self):
 
-        # Call the parent class and set up the window
+        # Creating the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+        #Create a scene object
+        self.scene = None
+
+        #This variable will hold the player's sprite
+        self.player_sprite = None
+
+        #Instantiating the physics engine
+        self.physics_engine = None
+
+        #Create a camera that can allow for traversing the screen more immersively
+        self.camera = None
+
+        #Create background attribute to store image in later
+        self.background = None
+
+        #Assign inbuilt sounds to attributes
+        self.collect_coin_sound = arcade.load_sound(":resources:sounds/coin1.wav")
+        self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
 
     def setup(self):
-        """Set up the game here. Call this function to restart the game."""
-        pass
+        
+        #Here we set up the camera
+        self.camera = arcade.Camera(self.width, self.height)
+
+        #Setting the color of the game window
+        self.background = arcade.load_texture(":resources:images/cybercity_background/back-buildings.png")
+
+        #Initialize the scene object here
+        self.scene = arcade.Scene()
+        self.scene.add_sprite_list("Player")
+        self.scene.add_sprite_list("Walls, use_spatial_hash=True")
+
+        #Initiate the players sprite
+        image_source = ":resources:images/animated_characters/robot/robot_idle.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 75
+        self.player_sprite.center_y = 150
+        self.scene.add_sprite("Player", self.player_sprite)
+
+        #Create the ground, using a loop to create the floor of the game window
+        for x in range(0, 1250, 64):
+            wall = arcade.Sprite(":resources:images/tiles/snowCenter.png", TILE_SCALING)
+            wall.center_x = x
+            wall.center_y = 32
+            self.scene.add_sprite("Walls", wall)
+
+        # Put some obstacles on the ground
+        coordinate_list = [[512, 96], [256, 96], [768, 96]]
+
+        for coordinate in coordinate_list:
+            # Add a crate on the ground
+            wall = arcade.Sprite(
+                ":resources:images/tiles/brickTextureWhite.png", TILE_SCALING
+            )
+            wall.position = coordinate
+            self.scene.add_sprite("Walls", wall)
+
+        #This will add the stars
+        for x in range(128, 1250, 256):
+            star = arcade.Sprite(":resources:images/items/star.png", STAR_SCALING)
+            star.center_x = x
+            star.center_y = 96
+            self.scene.add_sprite("Stars", star)
+
+        #Implement arcade's built-in physics engine to identify the sprites that cannot be moved through
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Walls"])
 
     def on_draw(self):
-        """Render the screen."""
-
+        #Clear the screen in case it's a level other than one
         self.clear()
-        # Code to draw the screen goes here
+
+        #Drawing the backgound image
+        arcade.draw_lrwh_rectangle_textured(0, 0,
+                                    SCREEN_WIDTH, SCREEN_HEIGHT,
+                                    self.background)
+
+        #Activate the earlier established camera attribute
+        self.camera.use()
+
+        # Draw the sprites onto the screen
+        self.scene.draw()
+
+    def on_key_press(self, key, modifiers):
+
+        #Here is where we assign player controls to WASD and assign positive and negative x and y values to those key presses
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = PLAYER_JUMP_SPEED
+            arcade.play_sound(self.jump_sound)
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        
+        #Here we assign a zero value to the directional value when the key is released
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+    
+    def center_camera_to_player(self):
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (
+            self.camera.viewport_height / 2
+        )
+
+        #This section of code ensures that the camera value does not travel past zero
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+        player_centered = screen_center_x, screen_center_y
+
+        self.camera.move_to(player_centered)
+
+    def on_update(self, delta_time):
+        
+        #This allows the scene to allow for multiple key presses and the utilization of the game engine 
+        self.physics_engine.update()
+
+        #Here we set up collision with the stars
+        star_list = arcade.check_for_collision_with_list(
+            self.player_sprite, self.scene["Stars"]
+        )
+
+        #Remove the stars we hit from the screen
+        for star in star_list:
+            star.remove_from_sprite_lists()
+            # Play a sound
+            arcade.play_sound(self.collect_coin_sound)
+
+        #Update the camera values
+        self.center_camera_to_player()
+        
 
 
 def main():
-    """Main function"""
     window = game()
     window.setup()
     arcade.run()
